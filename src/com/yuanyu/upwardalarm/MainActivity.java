64dcbba52fc6396a06bd1d0ad529c683a7a907ec
yuanyu0714 <yu.yuan@utbm.fr>
2014-03-07 00:02:52 +0100

    [implement] add alarm list item

--- src/com/yuanyu/upwardalarm/MainActivity.java
+++ src/com/yuanyu/upwardalarm/MainActivity.java
@@ -1,10 +1,13 @@
 package com.yuanyu.upwardalarm;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.app.ListActivity;
+import android.app.TimePickerDialog;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TimePicker;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,4 +22,22 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if(item.getItemId() == R.id.action_add) {
+			showTimePickerDialog();
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	private void showTimePickerDialog() {
+		TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener(){
+			@Override
+			public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+				// TODO
+			}
+		};
+		TimePickerDialog dialog = new TimePickerDialog(this, timeSetListener, 8, 0, true);
+		dialog.show();
+	}
 }
