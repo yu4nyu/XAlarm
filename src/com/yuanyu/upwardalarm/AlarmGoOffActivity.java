4b56370677a4c52e29841fa3e2b205e416e4b5e8
YUAN Yu <yuan@archos.com>
2014-04-08 18:06:14 +0200

    show a dialog when alarm goes off

--- src/com/yuanyu/upwardalarm/AlarmGoOffActivity.java
+++ src/com/yuanyu/upwardalarm/AlarmGoOffActivity.java
@@ -1,15 +1,41 @@
 package com.yuanyu.upwardalarm;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 
 public class AlarmGoOffActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_alarm_go_off);
 		
-		// TODO
+		AlarmGoOffDialog dialog = new AlarmGoOffDialog();
+		dialog.show(getFragmentManager(), "alarmGoOff");
+	}
+	
+	public static class AlarmGoOffDialog extends DialogFragment {
+
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setCancelable(false);
+		}
+
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			LayoutInflater inflater = LayoutInflater.from(getActivity());
+			View view = inflater.inflate(R.layout.dialog_alarm_go_off, null);
+			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+			builder.setView(view);
+			return builder.create();
+		}
 	}
 }
