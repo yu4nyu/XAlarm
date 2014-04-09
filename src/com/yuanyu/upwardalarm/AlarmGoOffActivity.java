8f8553f2dcb63a48beb7371fc2701f4723841bee
YUAN Yu <yuan@archos.com>
2014-04-09 17:41:32 +0200

    add test code for dispalying all the sensor data

--- src/com/yuanyu/upwardalarm/AlarmGoOffActivity.java
+++ src/com/yuanyu/upwardalarm/AlarmGoOffActivity.java
@@ -1,9 +1,18 @@
 package com.yuanyu.upwardalarm;
 
+import java.util.List;
+
+import com.yuanyu.upwardalarm.sensor.MovementTracker;
+import com.yuanyu.upwardalarm.sensor.MovementTracker.Sample;
+
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -18,11 +27,16 @@ public class AlarmGoOffActivity extends Activity {
 	private boolean mIsVibrate;
 	private String mRingtoneFile; // Absolute path of ringtone file, may be null
 	
+	// TODO delete this, for test only.
+	private TextView mText;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_alarm_go_off);
+		mText = (TextView) findViewById(R.id.activity_alarm_go_off_text);
+		mText.setMovementMethod(new ScrollingMovementMethod());
 		
 		Intent intent = getIntent();
 		mIsVibrate = intent.getBooleanExtra(AlarmBroadcastReceiver.EXTRA_IS_VIBRATE, false);
@@ -38,6 +52,8 @@ public class AlarmGoOffActivity extends Activity {
 	
 	public static class AlarmGoOffDialog extends DialogFragment {
 		
+		private MovementTracker mTracker;
+		
 		@Override
 		public void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
@@ -51,6 +67,9 @@ public class AlarmGoOffActivity extends Activity {
 			if(ringtone != null) {
 				startRingtone(ringtone);
 			}
+			
+			mTracker = new MovementTracker(getActivity());
+			mTracker.start();
 		}
 
 		@Override
@@ -59,6 +78,21 @@ public class AlarmGoOffActivity extends Activity {
 			View view = inflater.inflate(R.layout.dialog_alarm_go_off, null);
 			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 			builder.setView(view);
+			
+			
+			// For test
+			final Dialog dialog = builder.create();
+			final AlarmGoOffActivity activity = (AlarmGoOffActivity) getActivity();
+			Button button = (Button) view.findViewById(R.id.dialog_alarm_go_off_stop_button);
+			button.setOnClickListener(new OnClickListener(){
+				@Override
+				public void onClick(View v) {
+					mTracker.stop();
+					dialog.dismiss();
+					activity.showData(mTracker.getData());
+				}
+			});
+			
 			return builder.create();
 		}
 		
@@ -77,5 +111,9 @@ public class AlarmGoOffActivity extends Activity {
 		private void stopVibration() {
 			// TODO
 		}
+	}
+	
+	private void showData(List<Sample> data) {
+		
 	}
 }
