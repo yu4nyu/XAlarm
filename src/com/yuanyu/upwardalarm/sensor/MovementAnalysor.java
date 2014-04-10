b459783eff138c4a1cc1b611b6983c8fa2daa6f7
YUAN Yu <yuan@archos.com>
2014-04-10 14:06:25 +0200

    [implement] MovementAnalysor

--- src/com/yuanyu/upwardalarm/sensor/MovementAnalysor.java
+++ src/com/yuanyu/upwardalarm/sensor/MovementAnalysor.java
@@ -8,12 +8,41 @@ public enum MovementAnalysor {
 	
 	INSTANCE;
 
+	public static interface MovementListener {
+		void onUpwardDetected();
+	}
+	
+	// TODO verify these values
 	private final static int SUCCESSIVE_THRESHOLD = 5;
 	private final static float VALUE_THRESHOLD = 2.0f;
 	
 	private int mSuccessiveCount = 0;
 	
-	public boolean analyse(List<Sample> samples) {
-		return false; // TODO
+	private MovementListener mMovementListener;
+	
+	public void setMovementListener(MovementListener listener) {
+		mMovementListener = listener;
+	}
+	
+	public void removeMovementListener() {
+		mMovementListener = null;
+	}
+	
+	void analyse(List<Sample> samples) {
+		for(Sample sample : samples) {
+			if(sample.independentValue() <= VALUE_THRESHOLD) {
+				mSuccessiveCount++;
+			}
+			if(mSuccessiveCount >= SUCCESSIVE_THRESHOLD) {
+				notifyMovementListener();
+				mSuccessiveCount = 0;
+			}
+		}
+	}
+	
+	private void notifyMovementListener() {
+		if(mMovementListener != null) {
+			mMovementListener.onUpwardDetected();
+		}
 	}
 }
--- src/com/yuanyu/upwardalarm/sensor/MovementTracker.java
+++ src/com/yuanyu/upwardalarm/sensor/MovementTracker.java
@@ -1,6 +1,5 @@
 package com.yuanyu.upwardalarm.sensor;
 
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,8 +12,6 @@ import android.hardware.SensorManager;
 
 public class MovementTracker implements SensorEventListener {
 
-	// TODO add permission and feature requirement in manifest
-
 	private final static int TRACK_RATE = SensorManager.SENSOR_DELAY_NORMAL; // TODO verify this
 
 	private SensorManager mSensorManager;
@@ -25,29 +22,12 @@ public class MovementTracker implements SensorEventListener {
 		float y;
 		float z;
 		
-		// TODO delete this, for test only
-		public String toString() {
-			return "" + x + "  " + y + "  " + z + "  " + (x+y+z);
-		}
-		public String getSqrt() {
-			double result = Math.sqrt(x*x + y*y + z*z);
-			DecimalFormat form = new DecimalFormat("0.000");
-			return form.format(result);
-		}
-		public String getCalibratedSqrt() {
-			double sqrt = Math.sqrt(x*x + y*y + z*z);
-			if(mLastSqrt != Double.MAX_VALUE) {
-				double result = mLastSqrt * 0.9f + sqrt * 0.1f;
-				DecimalFormat form = new DecimalFormat("0.000");
-				return form.format(result);
-			}
-			mLastSqrt = sqrt;
-			return "";
+		public double independentValue() {
+			return Math.sqrt(x*x + y*y + z*z);
 		}
 	}
 	
 	private List<Sample> mData = new ArrayList<Sample>();
-	private static double mLastSqrt = Double.MAX_VALUE;
 
 	public MovementTracker(Context context) {
 		mSensorManager = (SensorManager) context.getSystemService(Service.SENSOR_SERVICE);
@@ -71,37 +51,15 @@ public class MovementTracker implements SensorEventListener {
 
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
+		// Do nothing here
 	}
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
-		// In this example, alpha is calculated as t / (t + dT),
-		// where t is the low-pass filter's time-constant and
-		// dT is the event delivery rate.
-
-		/*final float alpha = 0.8;
-
-		// Isolate the force of gravity with the low-pass filter.
-		gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
-		gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
-		gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
-
-		// Remove the gravity contribution with the high-pass filter.
-		linear_acceleration[0] = event.values[0] - gravity[0];
-		linear_acceleration[1] = event.values[1] - gravity[1];
-		linear_acceleration[2] = event.values[2] - gravity[2];*/
-
 		Sample sample = new Sample();
 		sample.x = event.values[0];
 		sample.y = event.values[1];
 		sample.z = event.values[2];
-		
-		DecimalFormat form = new DecimalFormat("0.000");
-		sample.x = Float.parseFloat(form.format(sample.x));
-		sample.y = Float.parseFloat(form.format(sample.y));
-		sample.z = Float.parseFloat(form.format(sample.z));
-		
 		mData.add(sample);
 	}
 }
