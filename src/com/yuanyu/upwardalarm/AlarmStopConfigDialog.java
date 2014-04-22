8ac068a797585338062b404f75555c0f891071f7
yuanyu0714 <yu.yuan@utbm.fr>
2014-04-22 22:06:38 +0200

    [improve] finish alarm stop config dialog

--- src/com/yuanyu/upwardalarm/AlarmStopConfigDialog.java
+++ src/com/yuanyu/upwardalarm/AlarmStopConfigDialog.java
@@ -9,13 +9,32 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class AlarmStopConfigDialog extends DialogFragment {
+	
+	public static interface OnAlarmStopConfiguredListener {
+		void onAlarmStopConfigured(int type, int level, int times);
+	}
+	
+	private OnAlarmStopConfiguredListener mOnAlarmStopConfiguredListener;
 	
 	private Spinner mSelectionSpinner;
 	private Spinner mLevelSpinner;
 	private SeekBar mTimesSeekBar;
+	private TextView mTimesText;
+
+	public void setOnAlarmStopConfiguredListener(OnAlarmStopConfiguredListener listener) {
+		mOnAlarmStopConfiguredListener = listener;
+	}
+	
+	private void notifyOnAlarmStopConfiguredListener(int type, int level, int times) {
+		if(mOnAlarmStopConfiguredListener != null) {
+			mOnAlarmStopConfiguredListener.onAlarmStopConfigured(type, level, times);
+		}
+	}
 
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -23,19 +42,35 @@ public class AlarmStopConfigDialog extends DialogFragment {
 		View view = inflater.inflate(R.layout.dialog_alarm_stop_config, null);
 		
 		mSelectionSpinner = (Spinner) view.findViewById(R.id.dialog_alarm_stop_config_selection_spinner);
-		String[] selections = getActivity().getResources().getStringArray(R.array.stop_selections);
-		ArrayAdapter<String> selectionAdapter = new ArrayAdapter<String>(
-				getActivity(), android.R.layout.simple_spinner_dropdown_item, selections);
+		ArrayAdapter<CharSequence> selectionAdapter = ArrayAdapter.createFromResource(
+				getActivity(), R.array.stop_selections, android.R.layout.simple_spinner_item);
+		selectionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		mSelectionSpinner.setAdapter(selectionAdapter);
 		
 		mLevelSpinner = (Spinner) view.findViewById(R.id.dialog_alarm_stop_config_level_spinner);
-		String[] levels = getActivity().getResources().getStringArray(R.array.level);
-		ArrayAdapter<String> levelAdapter = new ArrayAdapter<String>(
-				getActivity(), android.R.layout.simple_spinner_dropdown_item, levels);
+		ArrayAdapter<CharSequence> levelAdapter = ArrayAdapter.createFromResource(
+				getActivity(), R.array.level, android.R.layout.simple_spinner_item);
+		levelAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		mLevelSpinner.setAdapter(levelAdapter);
 		
+		mTimesText = (TextView) view.findViewById(R.id.dialog_alarm_stop_config_times_text);
+		mTimesText.setText(getString(R.string.times) + " x1");
+		
 		mTimesSeekBar = (SeekBar) view.findViewById(R.id.dialog_alarm_stop_config_seekbar);
-		// TODO
+		mTimesSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener(){
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+				mTimesText.setText(getString(R.string.times) + " x" + (progress + 1));
+			}
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				
+			}
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				
+			}
+		});
 		
 		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 		builder.setView(view)
@@ -43,7 +78,10 @@ public class AlarmStopConfigDialog extends DialogFragment {
 			.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
-					// TODO
+					int type = mSelectionSpinner.getSelectedItemPosition();
+					int level = mLevelSpinner.getSelectedItemPosition();
+					int times = mTimesSeekBar.getProgress();
+					notifyOnAlarmStopConfiguredListener(type, level, times);
 				}
 			});
 
