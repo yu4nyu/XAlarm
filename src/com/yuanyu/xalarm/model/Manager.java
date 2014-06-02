3ea8f064004d770e2a09e4a290f73680a6f359b1
YUAN Yu <yuan@archos.com>
2014-06-02 14:23:50 +0200

    [fix] alarms in XAlarm and XAlarm Pro may have the same id, that will cancel the alarm in other app if one alarm is set

--- src/com/yuanyu/xalarm/model/Manager.java
+++ src/com/yuanyu/xalarm/model/Manager.java
@@ -19,6 +19,7 @@ import java.util.List;
 import java.util.Locale;
 
 import com.yuanyu.xalarm.AlarmBroadcastReceiver;
+import com.yuanyu.xalarm.Configuration;
 
 import android.annotation.TargetApi;
 import android.app.AlarmManager;
@@ -43,6 +44,7 @@ public enum Manager {
 
 	final static String PREFS_KEY = "prefs";
 	private final static String INTENT_DATA_PREFIX = "com.yuanyu.xalarm:";
+	private final static String INTENT_DATA_PREFIX_PRO = "com.yuanyu.xalarmpro:";
 	private final static String PREFS_UNIQUE_ID_KEY = "unique_id";
 
 	private final static String ALARM_DATA_FILE_PREFIX = "alarm_data_";
@@ -93,7 +95,8 @@ public enum Manager {
 		}
 		
 		Intent intent = new Intent(context, AlarmBroadcastReceiver.class);
-		intent.setData(Uri.parse(INTENT_DATA_PREFIX + alarm.getId()));
+		String dataPrefix = Configuration.IS_PRO_VERSION ? INTENT_DATA_PREFIX_PRO : INTENT_DATA_PREFIX;
+		intent.setData(Uri.parse(dataPrefix + alarm.getId()));
 		intent.putExtra(AlarmBroadcastReceiver.EXTRA_ALARM_ID, alarm.getId());
 		intent.putExtra(AlarmBroadcastReceiver.EXTRA_ALARM_LABEL, alarm.getLabel());
 		intent.putExtra(AlarmBroadcastReceiver.EXTRA_IS_VIBRATE, alarm.getVibrateEnable());
@@ -127,7 +130,8 @@ public enum Manager {
 	 */
 	public void unregister(Context context, int alarmId) {
 		Intent intent = new Intent(context, AlarmBroadcastReceiver.class);
-		intent.setData(Uri.parse(INTENT_DATA_PREFIX + alarmId));
+		String dataPrefix = Configuration.IS_PRO_VERSION ? INTENT_DATA_PREFIX_PRO : INTENT_DATA_PREFIX;
+		intent.setData(Uri.parse(dataPrefix + alarmId));
 		PendingIntent alarmPending = PendingIntent.getBroadcast(context, alarmId, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 		AlarmManager alarmManager = (AlarmManager) context.getSystemService(Service.ALARM_SERVICE);
 		alarmManager.cancel(alarmPending);
